c00d5cc5b491779fc2c36e07fdfb1761f5e083d5
Jonathan Bustamante <jonakmex@gmail.com>
2022-08-18 11:39:51 -0400

    Improving the httpStatus ignore mechanism using the jsonignore annotation at root class level Response

--- condo-rest-api/src/main/java/com/jonak/condo/api/vm/HelloWorldResponse.java
+++ condo-rest-api/src/main/java/com/jonak/condo/api/vm/HelloWorldResponse.java
@@ -1,6 +1,5 @@
 package com.jonak.condo.api.vm;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper=false)
-@JsonIgnoreProperties({ "httpStatus" })
 public class HelloWorldResponse extends Response{
     public String greet;
 }
--- condo-rest-api/src/main/java/com/jonak/condo/api/vm/ValidationErrorResponse.java
+++ condo-rest-api/src/main/java/com/jonak/condo/api/vm/ValidationErrorResponse.java
@@ -1,6 +1,5 @@
 package com.jonak.condo.api.vm;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,7 +11,6 @@ import java.util.Map;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper=false)
-@JsonIgnoreProperties({ "httpStatus" })
 public class ValidationErrorResponse extends Response {
     public Map<String,String> validationErrors;
 }
